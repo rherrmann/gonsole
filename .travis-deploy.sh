763b912dc0dad2d15dd291760f4578a489f34113
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2014-05-31 16:39:30 +0200

    Attempt to deploy p2 repository to GitHub Pages, take four

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -3,6 +3,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   # create and cd into temporary deployment work directory
   cd $HOME
+echo -e "ls of $HOME:\n"
+ls -la
   mkdir deployment-work
   cd deployment-work
   
@@ -10,11 +12,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.email "travis-deployer@codeaffine.com"
   git config --global user.name "Travis Deployer"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/rherrmann/gonsole.git gh-pages > /dev/null 2>&1
+echo -e "after clone:\n"
+ls -la
 
   # copy the build result into the gh-pages repository
   mkdir -p repository 
   cp -Rf ../com.codeaffine.gonsole.releng/repository/target/repository/* ./repository
-
   # add, commit and push files
   git add -f .
   git commit -m "Deploy Travis build #$TRAVIS_BUILD_NUMBER to gh-pages"
