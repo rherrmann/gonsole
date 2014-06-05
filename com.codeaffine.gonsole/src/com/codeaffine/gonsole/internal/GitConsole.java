671e748aa5829eea715a3adb5b6dd06da563095c
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2014-06-05 23:41:26 +0200

    Equip the console page iself with an icon

--- com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsole.java
+++ com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsole.java
@@ -8,13 +8,17 @@ import org.eclipse.ui.console.IOConsole;
 import org.eclipse.ui.console.IOConsoleOutputStream;
 
 import com.codeaffine.gonsole.internal.repository.CompositeRepositoryProvider;
+import com.google.common.base.Charsets;
 
 public class GitConsole extends IOConsole {
+
+  private static final String TYPE = "com.codeaffine.gonsole.console";
+  private static final String ENCODING = Charsets.UTF_8.name();
 
   private final CompositeRepositoryProvider repositoryProvider;
 
   public GitConsole( CompositeRepositoryProvider repositoryProvider ) {
-    super( "Interactive Git Console", null );
+    super( "Interactive Git Console", TYPE, Icons.getDescriptor( Icons.GONSOLE ), ENCODING, true );
     this.repositoryProvider = repositoryProvider;
     Runnable runnable = new InputScanner();
     Thread thread = new Thread( runnable );
