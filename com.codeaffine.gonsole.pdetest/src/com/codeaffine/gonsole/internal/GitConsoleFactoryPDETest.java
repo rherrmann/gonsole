e24c2ed4c8435c55f0cdce250f6a56d82678d658
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2014-06-05 23:13:55 +0200

    Ensure that GitConsoleFactory reuses a previously created console

---
+++ com.codeaffine.gonsole.pdetest/src/com/codeaffine/gonsole/internal/GitConsoleFactoryPDETest.java
@@ -0,0 +1,51 @@
+package com.codeaffine.gonsole.internal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsoleManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GitConsoleFactoryPDETest {
+
+  private IConsoleManager consoleManager;
+  private GitConsoleFactory consoleFactory;
+
+  @Test
+  public void testOpenConsole() {
+    consoleFactory.openConsole();
+
+    assertThat( consoleManager.getConsoles() ).hasSize( 1 );
+    assertThat( consoleManager.getConsoles()[ 0 ] ).isInstanceOf( GitConsole.class );
+  }
+
+  @Test
+  public void testOpenConsoleTwice() {
+    consoleFactory.openConsole();
+
+    consoleFactory.openConsole();
+
+    assertThat( consoleManager.getConsoles() ).hasSize( 1 );
+    assertThat( consoleManager.getConsoles()[ 0 ] ).isInstanceOf( GitConsole.class );
+  }
+
+  @Before
+  public void setUp() {
+    consoleManager = spy( ConsolePlugin.getDefault().getConsoleManager() );
+    removeAllConsoles();
+    consoleFactory = new GitConsoleFactory();
+  }
+
+  @After
+  public void tearDown() {
+    removeAllConsoles();
+  }
+
+  private void removeAllConsoles() {
+    consoleManager.removeConsoles( consoleManager.getConsoles() );
+  }
+
+}
--- com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsoleFactory.java
+++ com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsoleFactory.java
@@ -1,24 +1,64 @@
 package com.codeaffine.gonsole.internal;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleFactory;
+import org.eclipse.ui.console.IConsoleManager;
 
 import com.codeaffine.gonsole.internal.repository.CompositeRepositoryProvider;
 import com.codeaffine.gonsole.internal.repository.TempRepositoryProvider;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 
 public class GitConsoleFactory implements IConsoleFactory {
 
+  private final IConsoleManager consoleManager;
+
+
+  public GitConsoleFactory() {
+    consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+  }
+
   @Override
   public void openConsole() {
+    GitConsole console = getConsole();
+    consoleManager.showConsoleView( console );
+  }
+
+  private GitConsole getConsole() {
+    GitConsole result = getExistingConsole();
+    if( result == null ) {
+      result = createConsole();
+    }
+    return result;
+  }
+
+  private GitConsole getExistingConsole() {
+    Predicate<IConsole> predicate = new Predicate<IConsole>() {
+      @Override
+      public boolean apply( IConsole input ) {
+        return input instanceof GitConsole;
+      }
+    };
+    return ( GitConsole )Iterables.find( getAllConsoles(), predicate, null );
+  }
+
+  private Collection<IConsole> getAllConsoles() {
+    return Arrays.asList( consoleManager.getConsoles() );
+  }
+
+  private static GitConsole createConsole() {
     TempRepositoryProvider tempRepositoryProvider = new TempRepositoryProvider();
     tempRepositoryProvider.ensureRepositories();
     CompositeRepositoryProvider repositoryProvider = new CompositeRepositoryProvider( tempRepositoryProvider );
     repositoryProvider.setCurrentRepositoryLocation( tempRepositoryProvider.getRepositoryLocations()[ 0 ] );
 
-    IConsole console = new GitConsole( repositoryProvider );
-    ConsolePlugin.getDefault().getConsoleManager().addConsoles( new IConsole[] { console } );
+    GitConsole result = new GitConsole( repositoryProvider );
+    ConsolePlugin.getDefault().getConsoleManager().addConsoles( new IConsole[] { result } );
+    return result;
   }
-
 }
