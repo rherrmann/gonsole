91a2816e038115cc861122ffcba4eeb1b0ff7e92
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2014-06-18 13:25:45 +0200

    Extract GitConsoleContentAssist

--- com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsolePage.java
+++ com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/GitConsolePage.java
@@ -2,38 +2,19 @@ package com.codeaffine.gonsole.internal;
 
 
 
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.commands.ActionHandler;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.DocumentEvent;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentPartitioner;
-import org.eclipse.jface.text.IDocumentPartitionerExtension;
-import org.eclipse.jface.text.ITypedRegion;
-import org.eclipse.jface.text.contentassist.ContentAssistant;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.TextConsolePage;
-import org.eclipse.ui.console.TextConsoleViewer;
-import org.eclipse.ui.handlers.IHandlerActivation;
-import org.eclipse.ui.handlers.IHandlerService;
-import org.eclipse.ui.internal.console.IOConsolePartition;
 import org.eclipse.ui.part.IPageBookViewPage;
 import org.eclipse.ui.part.IPageSite;
-import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
 
 import com.codeaffine.gonsole.internal.repository.CompositeRepositoryProvider;
 
 class GitConsolePage implements IPageBookViewPage {
 
-  private final TextConsolePage consolePage;
+  final TextConsolePage consolePage;
   private final InputObserver inputObserver;
 
   GitConsolePage( TextConsolePage consolePage,
@@ -52,74 +33,7 @@ class GitConsolePage implements IPageBookViewPage {
   @Override
   public void createControl( Composite parent ) {
     consolePage.createControl( parent );
-
-    TextConsoleViewer sourceViewer = consolePage.getViewer();
-
-    final ContentAssistant contentAssistant = new ContentAssistant();
-    contentAssistant.enablePrefixCompletion( true );
-    contentAssistant.setRepeatedInvocationMode( true );
-    final ContentAssistProcessor contentAssistProcessor = new ContentAssistProcessor();
-    contentAssistant.setContentAssistProcessor( contentAssistProcessor, IOConsolePartition.INPUT_PARTITION_TYPE );
-    contentAssistant.install( sourceViewer );
-    sourceViewer.getTextWidget().addDisposeListener( new DisposeListener() {
-      @Override
-      public void widgetDisposed( DisposeEvent event ) {
-        contentAssistant.uninstall();
-        contentAssistProcessor.dispose();
-      }
-    } );
-
-    sourceViewer.getTextWidget().addFocusListener( new FocusListener() {
-      private IHandlerActivation activation;
-
-      @Override
-      public void focusGained( FocusEvent event ) {
-        Action action = new Action() {
-          @Override
-          public void run() {
-            TextConsoleViewer viewer = consolePage.getViewer();
-            IDocument document = viewer.getDocument();
-
-            int caretOffset = viewer.getTextWidget().getCaretOffset();
-            if( caretOffset == viewer.getDocument().getLength() ) {
-              ITypedRegion partition = null;
-              try {
-                partition = document.getPartition( caretOffset );
-              } catch( BadLocationException ignore ) {
-              }
-              if( partition == null || !IOConsolePartition.INPUT_PARTITION_TYPE.equals( partition.getType() ) ) {
-                IDocumentPartitioner partitioner = document.getDocumentPartitioner();
-                if( partitioner instanceof IDocumentPartitionerExtension ) {
-                  IDocumentPartitionerExtension partitionerExtension = ( IDocumentPartitionerExtension )partitioner;
-                  DocumentEvent event = new DocumentEvent( document, caretOffset, 0, "" );
-                  partitionerExtension.documentChanged2( event );
-                }
-              }
-              }
-//            viewer.doOperation( ISourceViewer.CONTENTASSIST_PROPOSALS );
-            contentAssistant.showPossibleCompletions();
-          }
-        };
-        action.setActionDefinitionId( ITextEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS );
-        action.setEnabled( true );
-        String actionId = action.getActionDefinitionId();
-        ActionHandler handler = new ActionHandler( action );
-        IHandlerService handlerService = ( IHandlerService )PlatformUI.getWorkbench().getService( IHandlerService.class );
-        activation = handlerService.activateHandler( actionId, handler, null );
-      }
-
-      @Override
-      public void focusLost( FocusEvent event ) {
-        if( activation != null ) {
-          IHandlerService handlerService
-            = ( IHandlerService )PlatformUI.getWorkbench().getService( IHandlerService.class );
-          handlerService.deactivateHandler( activation );
-          activation = null;
-        }
-      }
-    } );
-
-
+    new GitConsoleContentAssist( consolePage.getViewer() ).install();
     inputObserver.start( consolePage.getViewer() );
   }
 
