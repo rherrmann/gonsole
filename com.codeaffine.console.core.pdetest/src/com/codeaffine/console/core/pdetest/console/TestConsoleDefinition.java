1eb3f3f33e9edd2eb708786519f5cd18010579d7
Frank Appel <fappel@eclipsesource.com>
2014-06-20 18:20:04 +0200

    Extract TestImageDescriptor
    - toDo: find a solution for code duplication

--- com.codeaffine.console.core.pdetest/src/com/codeaffine/console/core/pdetest/console/TestConsoleDefinition.java
+++ com.codeaffine.console.core.pdetest/src/com/codeaffine/console/core/pdetest/console/TestConsoleDefinition.java
@@ -3,23 +3,16 @@ package com.codeaffine.console.core.pdetest.console;
 import java.nio.charset.Charset;
 
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.PaletteData;
-import org.eclipse.swt.graphics.RGB;
 
 import com.codeaffine.console.core.ColorScheme;
 import com.codeaffine.console.core.ConsoleComponentFactory;
 import com.codeaffine.console.core.ConsoleDefinition;
+import com.codeaffine.console.core.pdetest.TestImageDescriptor;
 import com.google.common.base.Charsets;
 
 public class TestConsoleDefinition implements ConsoleDefinition {
 
-  public static ImageDescriptor IMAGE = new ImageDescriptor() {
-    @Override
-    public ImageData getImageData() {
-      return new ImageData(6, 6, 1, new PaletteData(new RGB[] { new RGB(255, 0, 0) }));
-    }
-  };
+  public static ImageDescriptor IMAGE = new TestImageDescriptor();
 
   @Override
   public String getType() {
---
+++ com.codeaffine.console.core.test/src/com/codeaffine/console/core/test/TestImageDescriptor.java
@@ -0,0 +1,14 @@
+package com.codeaffine.console.core.test;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.RGB;
+
+public class TestImageDescriptor extends ImageDescriptor {
+
+  @Override
+  public ImageData getImageData() {
+    return new ImageData(6, 6, 1, new PaletteData(new RGB[] { new RGB(255, 0, 0) }));
+  }
+}
