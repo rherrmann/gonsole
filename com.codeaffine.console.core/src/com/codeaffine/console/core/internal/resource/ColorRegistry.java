483b541cac154990608fd15a5c1619b508479004
Frank Appel <fappel@eclipsesource.com>
2014-06-20 18:19:08 +0200

    Use resource manager for ColorRegistry implementation

--- com.codeaffine.console.core/src/com/codeaffine/console/core/internal/resource/ColorRegistry.java
+++ com.codeaffine.console.core/src/com/codeaffine/console/core/internal/resource/ColorRegistry.java
@@ -3,52 +3,29 @@ package com.codeaffine.console.core.internal.resource;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 
-import java.util.Map;
-
-import org.eclipse.jface.resource.ColorDescriptor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
-import com.google.common.collect.Maps;
-
 class ColorRegistry {
 
-  private final Display display;
-  private final Map<RGB, Color> colors;
+  private final LocalResourceManager resourceManager;
 
   ColorRegistry( Display display ) {
     checkArgument( display != null, "Parameter 'display' must not be null." );
 
-    this.colors = Maps.newHashMap();
-    this.display = display;
-    registerDisposeHock( display );
+    this.resourceManager = new LocalResourceManager( JFaceResources.getResources( display ) );
   }
 
   Color getColor( RGB colorDefinition ) {
-    checkState( !display.isDisposed(), "Display has been disposed." );
+    checkState( !resourceManager.getDevice().isDisposed(), "Display has been disposed." );
 
-    Color result = colors.get( colorDefinition );
-    if( result == null ) {
-      result = ColorDescriptor.createFrom( colorDefinition ).createColor( display );
-      colors.put( colorDefinition, result );
-    }
-    return result;
+    return resourceManager.createColor( colorDefinition );
   }
 
   void clear() {
-    for( Color color : colors.values() ) {
-      color.dispose();
-    }
-    colors.clear();
-  }
-
-  private void registerDisposeHock( Display display ) {
-    display.disposeExec( new Runnable() {
-      @Override
-      public void run() {
-        clear();
-      }
-    } );
+    resourceManager.dispose();
   }
 }
