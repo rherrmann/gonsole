2d2fb23839108ce962aae8267b2b5e47988610eb
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2014-06-25 18:38:26 +0200

    Move console title end-to-end tests to its own class

--- com.codeaffine.gonsole.pdetest/src/com/codeaffine/gonsole/acceptance/GitConsoleLifecyclePDETest.java
+++ com.codeaffine.gonsole.pdetest/src/com/codeaffine/gonsole/acceptance/GitConsoleLifecyclePDETest.java
@@ -2,13 +2,10 @@ package com.codeaffine.gonsole.acceptance;
 
 import static com.codeaffine.console.core.pdetest.bot.ConsoleAssert.assertThat;
 import static com.codeaffine.gonsole.acceptance.GitConsolePrompts.line;
-import static org.assertj.core.api.Assertions.assertThat;
 
-import org.eclipse.ui.part.ViewPart;
 import org.junit.Rule;
 import org.junit.Test;
 
-import com.codeaffine.console.core.pdetest.ViewHelper;
 import com.codeaffine.console.core.pdetest.bot.ConsoleBot;
 
 
@@ -27,29 +24,5 @@ public class GitConsoleLifecyclePDETest {
     assertThat( consoleBot )
       .caretIsAtEnd()
       .containsLines( line( "repo", "status" ), "# On branch master" , line( "repo" ) );
-  }
-
-
-  @Test
-  public void testInitialConsoleViewTitle() {
-    consoleBot.open( configurer.createConfigurer( "repo" ) );
-
-    assertThat( getConsoleViewContentDescription() ).isEqualTo( "Git Console: repo" );
-  }
-
-  @Test
-  public void testConsoleViewTitleAfterChangingCurrentRepository() {
-    consoleBot.open( configurer.createConfigurer( "repo-1", "repo-2" ) );
-
-    consoleBot.enterCommandLine( "use repo-2" );
-
-    assertThat( consoleBot ).hasProcessedCommandLine();
-    assertThat( getConsoleViewContentDescription() ).isEqualTo( "Git Console: repo-2" );
-  }
-
-
-  private static String getConsoleViewContentDescription() {
-    ViewPart consoleView = new ViewHelper().showView( ConsoleBot.CONSOLE_VIEW_ID );
-    return consoleView.getContentDescription();
   }
 }
