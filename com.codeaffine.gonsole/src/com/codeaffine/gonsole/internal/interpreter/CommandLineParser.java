rherrmann/gonsole

commit 461d51932017043fc6db0fd1fd85109f50121e58

Add description for the 'use' command

com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/interpreter/CommandLineParser.java
@@ -49,6 +49,8 @@ public class CommandLineParser {
   private String getUsage( CommandInfo commandInfo ) {
     StringWriter result = new StringWriter();
     CmdLineParser parser = new CmdLineParser( commandInfo.getCommand() );
+    parser.printSingleLineUsage( result, pgmResourceBundle.getResourceBundle() );
+    result.append( "\n\n" );
     parser.printUsage( result, pgmResourceBundle.getResourceBundle() );
     return result.toString();
   }

com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/contentassist/ControlCommandContentProposalProvider.java
@@ -8,7 +8,13 @@ import com.codeaffine.gonsole.internal.activator.IconRegistry;
 
 public class ControlCommandContentProposalProvider implements ContentProposalProvider {
 
-  static final Proposal[] PROPOSALS = new Proposal[] { new Proposal( "use", "ToDo" ) };
+  private static final String USAGE
+    = "use repository - Change the current repository\n\n"
+    + "repository: Name of the repository to use. "
+    + "The name is taken from the last segment of the path to the repository (without .git). "
+    + "E.g. the name of /path/to/repo/.git is 'repo'.";
+
+  static final Proposal[] PROPOSALS = new Proposal[] { new Proposal( "use", USAGE ) };
 
   @Override
   public Proposal[] getContentProposals() {

commit 8dfe5844cf17207a32df84aa944e46e3686054ec

Fix failing test that was broken with 461d51932017043fc6db0fd1fd85109f50121e58

com.codeaffine.gonsole/src/com/codeaffine/gonsole/internal/interpreter/CommandLineParser.java
@@ -50,7 +50,9 @@ public class CommandLineParser {
     StringWriter result = new StringWriter();
     CmdLineParser parser = new CmdLineParser( commandInfo.getCommand() );
     parser.printSingleLineUsage( result, pgmResourceBundle.getResourceBundle() );
-    result.append( "\n\n" );
+    if( result.getBuffer().length() > 0 ) {
+      result.append( "\n\n" );
+    }
     parser.printUsage( result, pgmResourceBundle.getResourceBundle() );
     return result.toString();
   }