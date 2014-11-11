f370f258c20bea2ff107e3c860e735391e8c8ba9
Frank Appel <fappel@eclipsesource.com>
2014-11-11 10:20:06 +0100

    Use com.codeaffine.osgi.testsuite 1.1.0 for integration tests
    - update targets to com.codeaffine.osgi.testsuite 1.1.0
    - remove obsolete osgi-testsuite hack in test.util fragment
    - update AllPDETests to use ClassnameFilters

--- com.codeaffine.gonsole.test.all/src/com/codeaffine/gonsole/test/all/AllPDETests.java
+++ com.codeaffine.gonsole.test.all/src/com/codeaffine/gonsole/test/all/AllPDETests.java
@@ -2,14 +2,19 @@ package com.codeaffine.gonsole.test.all;
 
 import org.junit.runner.RunWith;
 
-import com.codeaffine.gonsole.test.util.osgi.testsuite.BundleTestSuite;
-import com.codeaffine.gonsole.test.util.osgi.testsuite.BundleTestSuite.TestBundles;
+import com.codeaffine.osgi.testuite.BundleTestSuite;
+import com.codeaffine.osgi.testuite.BundleTestSuite.ClassnameFilters;
+import com.codeaffine.osgi.testuite.BundleTestSuite.TestBundles;
 
 @RunWith(BundleTestSuite.class)
-@TestBundles({
+@TestBundles( {
 	"com.codeaffine.console.core",
 	"com.codeaffine.gonsole",
 	"com.codeaffine.gonsole.egit",
-	"com.codeaffine.console.calculator" })
+	"com.codeaffine.console.calculator"
+} )
+@ClassnameFilters( {
+  ".*PDETest"
+} )
 public class AllPDETests {
 }
