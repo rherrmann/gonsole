f5aca572bcabf55e6d050862d24a57c8092c29f3
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2016-01-20 20:30:29 +0100

    Update target platform

--- com.codeaffine.console.core/src/com/codeaffine/console/core/ConsoleConstants.java
+++ com.codeaffine.console.core/src/com/codeaffine/console/core/ConsoleConstants.java
@@ -1,12 +1,11 @@
 package com.codeaffine.console.core;
 
 import java.nio.charset.Charset;
-
-import com.google.common.base.Charsets;
+import java.nio.charset.StandardCharsets;
 
 
 public class ConsoleConstants {
 
-  public static final Charset ENCODING = Charsets.UTF_8;
+  public static final Charset ENCODING = StandardCharsets.UTF_8;
   public static final String LINE_DELIMITER = System.getProperty( "line.separator" );
 }
